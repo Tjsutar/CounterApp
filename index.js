569c5ab0b309e2b4fbc37b580b6acc2bceaa4a39
Tushar Sutar <101010382+Tjsutar@users.noreply.github.com>
2023-03-31 19:15:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,3 @@
-// const daysElement = document.querySelector(".days");
-// const hoursElement = document.querySelector(".hours");
-// const minutesElement = document.querySelector(".minutes");
-// const secsElement = document.querySelector(".secs");
-// const heading = document.querySelector("h1");
-// const counterTimer = document.querySelector(".timer");
-
-// // Converting second, minute, hour, day in milliseconds
-// const second = 1000,
-//     minute = 60 * second,
-//     hour = 60 * minute,
-//     day = 24 * hour;
-
-// const timerFunction = () => {
-//     // Generating current date in dd/mm/yyyy format
-//     const now = new Date();
-//     const dd = String(now.getDate()).padStart(2, "0");
-//     const mm = String(now.getMonth() + 1).padStart(2, "0");
-//     const yyyy = now.getFullYear();
-//     const currentDate = `${mm}/${dd}/${yyyy}`;
-
-//     // Taking target date from user
-//     let enteredDay = prompt("Enter Day").padStart(2, "0");
-//     enteredDay = enteredDay > 31 ? prompt("Enter less than 31") : enteredDay;
-//     let enteredMonth = prompt("Enter Month").padStart(2, "0");
-//     enteredMonth = enteredMonth > 12 ? prompt("Enter less than 12") : enteredMonth;
-//     const targetDate = new Date(`${yyyy}-${enteredMonth}-${enteredDay}`);
-//     const targetTime = targetDate.getTime();
-//     const nextYearTime = new Date(`${yyyy + 1}-01-01`).getTime();
-//     const timer = targetTime < now.getTime() ? nextYearTime + targetTime : targetTime;
-
-//     const intervalId = setInterval(() => {
-//         // Taking current date in milliseconds
-//         const today = new Date().getTime();
-
-//         // Finding difference between target date and today's date
-//         let difference = timer - today;
-
-//         // Finding left day, hour, minute, second
-//         const leftDay = Math.floor(difference / day);
-//         const leftHour = Math.floor((difference % day) / hour);
-//         const leftMinute = Math.floor((difference % hour) / minute);
-//         const leftSecond = Math.floor((difference % minute) / second);
-
-//         // Showing timer in the DOM
-//         daysElement.innerText = leftDay;
-//         hoursElement.innerText = leftHour;
-//         minutesElement.innerText = leftMinute;
-//         secsElement.innerText = leftSecond;
-
-//         // Stopping setInterval after reaching the target time
-//         if (difference <= 0) {
-//             counterTimer.style.display = "none";
-//             heading.innerText = "Time's Over";
-//             clearInterval(intervalId);
-//         }
-//     }, 0);
-// };
-
-// timerFunction();
-
 const daysElement = document.querySelector(".days");
 const hoursElement = document.querySelector(".hours");
 const minutesElement = document.querySelector(".minutes");
@@ -73,31 +12,32 @@ const second = 1000,
     day = 24 * hour;
 
 const timerFunction = () => {
-    // Generating current date in dd/mm/yyyy format
+    // Generating current date in mm/dd/yyyy format
     const now = new Date();
-    const dd = String(now.getDate()).padStart(2, "0");
     const mm = String(now.getMonth() + 1).padStart(2, "0");
+    const dd = String(now.getDate()).padStart(2, "0");
     const yyyy = now.getFullYear();
     const currentDate = `${mm}/${dd}/${yyyy}`;
 
     // Taking target date from user
-    let enteredDay = prompt("Enter Day").padStart(2, "0");
+    let enteredDay = parseInt(prompt("Enter Day").padStart(2, "0"));
     while (enteredDay > 31) {
-        enteredDay = prompt("Enter a day less than or equal to 31").padStart(2, "0");
+        enteredDay = parseInt(prompt("Enter a day less than or equal to 31").padStart(2, "0"));
     }
-    let enteredMonth = prompt("Enter Month").padStart(2, "0");
-    enteredMonth = enteredMonth > 12 ? prompt("Enter less than 12") : enteredMonth;
-    const targetDate = new Date(`${yyyy}-${enteredMonth}-${enteredDay}`);
-    const targetTime = targetDate.getTime();
-    const nextYearTime = new Date(`${yyyy + 1}-01-01`).getTime();
-    const timer = targetTime < now.getTime() ? nextYearTime + targetTime : targetTime;
+    let enteredMonth = parseInt(prompt("Enter Month").padStart(2, "0"));
+    enteredMonth = enteredMonth > 12 ? parseInt(prompt("Enter a month less than or equal to 12").padStart(2, "0")) : enteredMonth;
+    let targetYear = yyyy;
+    if (enteredMonth < mm || (enteredMonth === mm && enteredDay < dd)) {
+        targetYear++;
+    }
+    const targetDate = new Date(`${enteredMonth}/${enteredDay}/${targetYear}`).getTime();
 
     const intervalId = setInterval(() => {
         // Taking current date in milliseconds
         const today = new Date().getTime();
 
         // Finding difference between target date and today's date
-        let difference = timer - today;
+        let difference = targetDate - today;
 
         // Finding left day, hour, minute, second
         const leftDay = Math.floor(difference / day);
@@ -121,4 +61,3 @@ const timerFunction = () => {
 };
 
 timerFunction();
-
